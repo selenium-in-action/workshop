fc4c2a5f664792f38a12d3c56fed5c3a5d6bf5b1
Roy de Kleijn <dekleijn.roy@gmail.com>
2023-09-18 08:21:21 +0200

    change

--- src/test/java/browser/BaseTest.java
+++ src/test/java/browser/BaseTest.java
@@ -3,14 +3,11 @@ package browser;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Listeners;
-import utils.listeners.TestListener;
 import utils.webdriver.Browser;
 import utils.webdriver.BrowserProvider;
 
 import java.net.MalformedURLException;
 
-@Listeners({TestListener.class})
 public class BaseTest {
 
     private WebDriver driver;
